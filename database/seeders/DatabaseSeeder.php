77ae2da02b6bc2b6edc7605988e702158fd2983e
Hossam Soliuman <hossamsoliuman@gmail.com>
2026-06-03 17:58:50 +0300

    feat: add demo issue data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(DefaultMasterSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(DemoDataSeeder::class);
+        $this->call(AccountingErrorsDemoSeeder::class);
     }
 }
