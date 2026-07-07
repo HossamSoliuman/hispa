a317bc4300da1b951d441a2531331b75697c591d
Hossam Soliuman <hossamsoliuman@gmail.com>
2026-07-07 06:54:04 +0300

    refactor: clean schedule

--- routes/console.php
+++ routes/console.php
@@ -4,14 +4,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-Schedule::command('app:assets-depreciation-run')
-    ->yearlyOn(1, 1)
-    ->at('01:00');
-
 Schedule::command('db:backup')
     ->everyMinute()
     // ->at('02:00')
